0e49156af7410766bc474961a295006207e1d12c
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-05-05 05:39:55 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,10 @@
 def using_until
   levitation_force = 6
   #your code here
-  counter = 0
-  until counter ==6
+  
+  until levitation_force == 10
     puts "Wingardium Leviosa"
-    counter += 1
+    levitation_force += 1
   end
 
 end
